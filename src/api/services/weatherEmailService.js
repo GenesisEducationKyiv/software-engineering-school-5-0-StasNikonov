1b2a303dfab281bf8a6bf26ffd0de636a6cc5af4
StasNikonov <nikonoff.stanislav2005@gmail.com>
2025-06-15 09:22:55 +0300

    Apply SOLID & GRASP principles in hw3

--- src/api/services/weatherEmailService.js
+++ src/api/services/weatherEmailService.js
@@ -1,41 +1,33 @@
-const { Subscription } = require('../../db/models');
-const { getWeather } = require('./weatherService');
-const { forecastEmail } = require('../../utils/forecastTemplates');
-const transporter = require('./mailer');
+const { prepareEmailHtml } = require('../../utils/prepareEmailHtml');
 
-const sendWeatherEmailToSubscribers = async (frequency) => {
+const sendWeatherEmailToSubscribers = async (
+  frequency,
+  getWeather,
+  transporter,
+  db,
+) => {
   try {
-    const subscribers = await Subscription.findAll({
-      where: { confirmed: true, frequency },
-    });
+    const subscribers = await db.getConfirmedByFrequency(frequency);
 
     for (let subscriber of subscribers) {
-      const { email, city, token } = subscriber;
-
       try {
-        const weather = await getWeather(city);
-
-        const unsubscribeLink = `${process.env.BASE_URL}/api/unsubscribe/${token}`;
-
-        const html = forecastEmail({
-          city,
-          temp_c: weather.temperature,
-          humidity: weather.humidity,
-          condition: weather.description,
-          unsubscribeLink,
-        });
+        const weather = await getWeather(subscriber.city);
+        const html = prepareEmailHtml(subscriber, weather);
 
         const mailOptions = {
           from: process.env.EMAIL_USER,
-          to: email,
-          subject: `Погода у місті ${city}`,
+          to: subscriber.email,
+          subject: `Погода у місті ${subscriber.city}`,
           html,
         };
 
         await transporter.sendMail(mailOptions);
-        console.log(`✅ Email sent to ${email} (${frequency})`);
+        console.log(`✅ Email sent to ${subscriber.email} (${frequency})`);
       } catch (err) {
-        console.error(`❌ Failed to send email to ${email}:`, err.message);
+        console.error(
+          `❌ Failed to send email to ${subscriber.email}:`,
+          err.message,
+        );
       }
     }
   } catch (error) {
