a1bfeca1ad188b14da0794b8c20f32baa088e1e9
StasNikonov <nikonoff.stanislav2005@gmail.com>
2025-06-26 00:01:17 +0300

    fix: resolve lint warnings and errors

--- src/api/providers/AbstractWeatherProvider.js
+++ src/api/providers/AbstractWeatherProvider.js
@@ -5,6 +5,7 @@ class AbstractWeatherProvider {
   }
 
   async fetch(city) {
+    this._city = city;
     throw new Error('fetch(city) not implemented');
   }
 }
--- src/api/services/cityValidation/AbstractCityValidator.js
+++ src/api/services/cityValidation/AbstractCityValidator.js
@@ -5,6 +5,7 @@ class AbstractCityValidator {
   }
 
   async validateCity(city) {
+    this._city = city;
     throw new Error('validateCity(city) not implemented');
   }
 }
